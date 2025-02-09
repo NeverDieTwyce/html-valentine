1f555075ff4e40777852b27d3ea7d640ceedca96
NeverDieTwyce <121199290+NeverDieTwyce@users.noreply.github.com>
2025-02-09 22:38:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Tu es sûr ?",
+    "Vraiment sûr ??",
+    "Tu es certain ?",
+    "Pookie s'il te plaît...",
+    "Juste, réfléchis-y !",
+    "Si tu dis non, je serai vraiment triste...",
+    "Je serai très triste...",
+    "Je serai très très très triste...",
+    "Ok d'accord, j'arrête de demander...",
+    "Je rigole, dis oui s'il te plaît ! ❤️"
 ];
 
 let messageIndex = 0;
